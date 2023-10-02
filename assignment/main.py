265ef37fba55b61ae06f0fbd3dfc0e3de39cf226
somanathan00 <113913322+somanathan00@users.noreply.github.com>
2023-10-02 20:36:16 +0530

    Update main.py

--- assignment/main.py
+++ assignment/main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+# Load the CSV
 df = pd.read_csv('Assignment_Timecard.xlsx - Sheet1.csv')
 
+# Validate time formats before converting
 try:
     df['Start'] = pd.to_datetime(df['Time'], format="%m/%d/%Y %I:%M %p")
     df['End'] = pd.to_datetime(df['Time Out'], format="%m/%d/%Y %I:%M %p")
@@ -25,13 +27,21 @@ names_shift_diff = df_sorted['Employee Name'][(df_sorted['Between_Shifts'] > 1) 
 # c) More than 14 hours in a single shift
 names_14_hours = df['Employee Name'][df['Duration'] > 14].unique()
 
-# Display results
-print("Employees who worked for 7 consecutive days:", names_7_days)
-print("Employees with less than 10 hours but more than 1 hour between shifts:", names_shift_diff.tolist())
-print("Employees who worked more than 14 hours in a single shift:", names_14_hours.tolist())
-
 # Check for overlapping shifts
 overlapping_shifts = df_sorted[df_sorted.duplicated(subset=['Employee Name', 'Start'], keep=False) | df_sorted.duplicated(subset=['Employee Name', 'End'], keep=False)]
-if not overlapping_shifts.empty:
-    print("\nWarning: Found potential overlapping shifts for the following employees:")
-    print(overlapping_shifts[['Employee Name', 'Start', 'End']])
+
+# Write results to output.txt
+with open('output.txt', 'w') as file:
+    file.write("Employees who worked for 7 consecutive days:\n")
+    file.write("\n".join(names_7_days))
+    
+    file.write("\n\nEmployees with less than 10 hours but more than 1 hour between shifts:\n")
+    file.write("\n".join(names_shift_diff.tolist()))
+    
+    file.write("\n\nEmployees who worked more than 14 hours in a single shift:\n")
+    file.write("\n".join(names_14_hours.tolist()))
+
+    if not overlapping_shifts.empty:
+        file.write("\n\nWarning: Found potential overlapping shifts for the following employees:\n")
+        overlapping_data = overlapping_shifts[['Employee Name', 'Start', 'End']].to_string(index=False)
+        file.write(overlapping_data)
